85383f1308ebd53b14d565e411975b5b265154d9
yuichi-morisaki <yuichi.morisaki@gmail.com>
2020-11-19 23:55:56 +0900

    Add the first example code

---
+++ ch02-helloworld/hello.cpp
@@ -0,0 +1,11 @@
+// This program outputs the message "Hello, World!" to the monitor.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!\n";  // Character OUTput stream
+    return 0;
+}
