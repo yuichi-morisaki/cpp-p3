7b1968a5c89b82ae53d1aa52dd501dba01cc21ed
yuichi-morisaki <yuichi.morisaki@gmail.com>
2020-11-20 16:02:46 +0900

    Add exercises in chapter 2

---
+++ ch02-helloworld/ex1.cpp
@@ -0,0 +1,12 @@
+// This program outputs the message "Hello, World!" to the monitor.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, programming!\n";  // Character OUTput stream
+    cout << "Here we go!\n";
+    return 0;
+}
